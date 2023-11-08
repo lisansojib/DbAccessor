6050a670a7ff142013844e06522765624b90620e
MD. SOJIB MIYA <md.sojibmiya@gmail.com>
2023-11-09 01:09:12 +0600

    UserStatistics model added

---
+++ src/ApplicationCore/Models/UserStatistics.cs
@@ -0,0 +1,19 @@
+﻿namespace ApplicationCore.Models
+{
+    public class UserStatistics
+    {
+        public int Id { get; set; } 
+        public int UserId { get; set; }
+        public int TeamId { get; set; }
+
+        /// <summary>
+        /// Get name from <see cref="UserStatisticsTypes"/> enum.
+        /// </summary>
+        public string StatisticsType { get; set; }
+        public int Count { get; set; }
+        public int Quota { get; set; }
+        public DateTime LastUpdateOn { get; set; } = DateTime.Now;
+        public DateTime? LastMonthlyCycleStart { get; set; }
+        public DateTime? LastMonthlyCycleEnd { get; set; }
+    }
+}
