4d3d8e94d8535e8ec56171722052680d7df666f9
MD. SOJIB MIYA <md.sojibmiya@gmail.com>
2023-11-09 00:02:43 +0600

    DbConfig added

---
+++ src/ApplicationCore/Configs/DbConfig.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace ApplicationCore.Configs
+{
+    public class DbConfig
+    {
+        public string ConnectionString { get; set; }
+        public SqlConnection Connection => new(ConnectionString);
+    }
+}
