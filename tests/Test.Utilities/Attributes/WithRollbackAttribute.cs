257906bc6661c1449382ed093c7138a266127054
MD. SOJIB MIYA <md.sojibmiya@gmail.com>
2023-11-09 01:11:43 +0600

    Test.Utilities project added

---
+++ tests/Test.Utilities/Attributes/WithRollbackAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Transactions;
+using Xunit.Sdk;
+
+namespace Test.Utilities.Attributes
+{
+    public class WithRollbackAttribute : BeforeAfterTestAttribute
+    {
+        private TransactionScope _scope;
+
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            _scope = new TransactionScope(TransactionScopeOption.Required,
+                new TransactionOptions
+                {
+                    IsolationLevel = IsolationLevel.ReadCommitted,
+                    Timeout = TransactionManager.MaximumTimeout,
+                },
+                TransactionScopeAsyncFlowOption.Enabled
+            );
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            _scope.Dispose();
+        }
+    }
+}
